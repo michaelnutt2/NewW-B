f2ad16e8d58bfb6af1ac8b8df01b662966330765
Michael Nutt <michaelnutt2@my.unt.edu>
2019-10-29 04:31:23 -0500

    Adding MongoDB to app.js and setting up controller

--- express-neww-b/app.js
+++ express-neww-b/app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://newsDev:newB@10.125.187.72:9002/news';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
