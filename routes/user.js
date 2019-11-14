df103dc1c4c4b0029aa5b0446f28c92619ab5125
Marks <Constant.Marks@unt.edu>
2019-11-14 14:38:28 -0600

    added some usr and password controls

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,8 @@ var user_controller = require('../controllers/userController');
 // GET home page and user login form defualts
 router.get('/', user_controller.user_profile);
 router.post('/', user_controller.user_profile);
+router.post('/mod_user', user_controller.mod_user);
+router.post('/change_pass', user_controller.change_pass);
+router.post('/delete', user_controller.delete);
 
 module.exports = router;
